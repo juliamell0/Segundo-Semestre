7cd2f680f735e047a066974fdfc0fc102141d1a0
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-04 16:51:13 -0300

    Salvando

---
+++ react-basico/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import InfoAluno from './components/InfoAluno'
+import AdicionarAluno from './components/AdicionarAluno'
+
+function App() {
+
+
+  return (
+    <>
+      <InfoAluno/>
+    </>
+  )
+}
+
+export default App
